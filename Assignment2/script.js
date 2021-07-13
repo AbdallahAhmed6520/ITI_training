c7eb2a057cbfeaef358a42c2a4bb12c84eb9df88
abdosedroho <abdosedroho@gmail.com>
2021-07-13 21:53:34 +0200

    sol Assignment2

---
+++ Assignment2/script.js
@@ -0,0 +1,8 @@
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+function Sum(x, y) {
+    return x + y;
+}
+
+alert(Sum(num1, num2));
