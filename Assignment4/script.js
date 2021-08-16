d05ce6172da6b331d913c0622bcab4f9c249466f
abdosedroho <abdosedroho@gmail.com>
2021-08-16 08:54:01 +0200

    sol of Assignment4

---
+++ Assignment4/script.js
@@ -0,0 +1,12 @@
+do
+{
+  var Name =prompt("enter your name");
+}
+  while( !isNaN(Name))
+
+  do{
+      var birthyear =prompt("enter your birthyear");
+  }
+  while(isNaN(birthyear)&&birthyear>2010)
+
+  document.write("Name :",Name,"<br> Birthyear : ",birthyear,"<br> Age = ",2021-birthyear)
